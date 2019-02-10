81928e0eca228d682197b76c961b1a6b17359a3d
Mark Dyehouse <markdyehouse2019@u.northwestern.edu>
2019-02-11 07:23:00 +0900

    Getting a rough estimate for distance fed back into UART for debugging on computer, needs tuning but roughly accurate

--- talkingPIC.c
+++ talkingPIC.c
@@ -9,6 +9,7 @@
 
 void delay(void);
 unsigned int sendPulse(void);
+unsigned int getCount(void);
 
 int main(void) {
   char message[MAX_MESSAGE_LENGTH];
@@ -31,7 +32,7 @@ int main(void) {
     count = sendPulse();
     //if (count != 2634039641) {
       //sprintf(strToWrite, "%u",count);//"%6.4f", count);
-    sprintf(strToWrite, "%u", PORTDbits.RD4);
+    sprintf(strToWrite, "%6.4f", ((float)count)/25*343/1000000000);// PORTDbits.RD4);
     NU32_WriteUART3(strToWrite);                  // send message back
     NU32_WriteUART3("\r\n");                      // carriage return and newline
     NU32_LED1 = !NU32_LED1;                       // toggle the LEDs
@@ -45,9 +46,9 @@ int main(void) {
 unsigned int sendPulse(){
   int j;
   unsigned int start, finish;
-  float count = 0.0; // turn this into something where I am using the SYSCLK and getting time from freq and ticks
+  unsigned int count = 0.0; // turn this into something where I am using the SYSCLK and getting time from freq and ticks
 
-  if (!PORTDbits.RD7) {
+  //if (!PORTDbits.RD7) {
     // Pin D7 is the USER switch, low (FALSE) if pressed.
     //LATDbits.LATD3 = 0;
     //WAITING = 1;
@@ -55,11 +56,12 @@ unsigned int sendPulse(){
     //for (j = 0; j < 400; j++) {
     //}
     //start = _CP0_GET_COUNT();
-    LATDbits.LATD3 = 1;
-    for (j = 0; j < 500; j++) {
-      Nop();
-    }
-    LATDbits.LATD3 = 0;
+  LATDbits.LATD3 = 1;
+  for (j = 0; j < 500; j++) {
+    Nop();
+  }
+  LATDbits.LATD3 = 0;
+  count = getCount();
 
     //finish = _CP0_GET_COUNT();
     //count = (float)((finish-start)*25/1000000000); // this just gives approx 0.1 sec
@@ -67,8 +69,8 @@ unsigned int sendPulse(){
     // same as /40,000,000
     // assumes less than 2 * 1 cycle (107 sec each) has passed
 
-  }
-  return finish-start;
+  //}
+  return count;
 }
 
 void delay(void) {
@@ -78,4 +80,19 @@ void delay(void) {
         ;   // Pin D7 is the USER switch, low (FALSE) if pressed.
     }
   }
+}
+
+unsigned int getCount(void){
+  unsigned int start = 0, fin = 0;
+  while (!PORTDbits.RD4) {
+    Nop();
+    // watch out for getting caught in these loops if something gets interrupted
+    // and then the switch happens quickly before this loop is entered... prevent this
+  }
+  start = _CP0_GET_COUNT();
+  while (PORTDbits.RD4) {
+    Nop();
+  }
+  fin = _CP0_GET_COUNT();
+  return fin - start;
 }
